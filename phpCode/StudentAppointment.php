faba4791c7d49238187bbd9165782e17774b783d
NadezhdaBzhilyanskaya <nadezhda.bzhilyanskaya@gmail.com>
2016-10-07 11:28:29 -0400

    Update to the Student appointment its a bit prettier now and uses advisor database as well

--- phpCode/StudentAppointment.php
+++ phpCode/StudentAppointment.php
@@ -6,7 +6,7 @@ $COMMON = new Common($debug);
 
 //$student= mysql_real_escape_string($_SESSION["key"]);
 
-$student = 2;
+$student = 1;
 
 $currentAppointment = "SELECT * FROM `meetings` WHERE `student1` = " . $student;
 for($i = 2;$i<=10; $i+=1)
@@ -18,46 +18,65 @@ $results = $COMMON->executeQuery($currentAppointment, $_SERVER["SCRIPT_NAME"]);
 
 if($rows = mysql_fetch_array($results))
 {
-  while ($rows = mysql_fetch_array($results)){
-    $advisor = $rows['advisor'];
-    $isGroup = $rows['isGroup'];
-    $location = $rows['location'];
-    $dateTime = $rows['dateTime'];
 
-    echo ("You already have an appointment:<br>\t");
-    if($isGroup){
-      echo ("Group ");
-    }
-    else{
-      echo ("Individual ");}
+  $query = "SELECT * FROM `staff` WHERE `key` =".$rows['advisor'];
+  $staffRS = $COMMON->executeQuery($query, $_SERVER["SCRIPT_NAME"]);
+  $staffInfo  = mysql_fetch_array($staffRS);
 
-    echo("with ". $advisor. " in ".$location." at ". $dateTime.".");
+  $advisor = ($staffInfo['fname']. " ".$staffInfo['lname']);
+  $isGroup = $rows['isGroup'];
+  $location = $rows['location'];
+  $dateTime = $rows['dateTime'];
 
+  echo ("You already have an appointment:<br>\t");
+  if($isGroup){
+    echo ("Group ". str_repeat("&nbsp;", 10));
   }
+  else{
+    echo ("Individual ".str_repeat("&nbsp;", 5));}
+  
+  echo(" with ".$advisor. " in ".$location." at ". $dateTime.".");
 
 }
 else
 {
-  $query = "SELECT * FROM `meetings` WHERE `numStudentsRegistered` = 0";
+  $query = "SELECT * FROM `meetings` WHERE `numStudentsRegistered` = 0 OR (`isGroup`= 1 AND `numStudentsRegistered` < 10)";
   $results = $COMMON->executeQuery($query, $_SERVER["SCRIPT_NAME"]);
 
-  echo ("Availible appointments are:<br>\t"); 
+  if($rows = mysql_fetch_array($results)){
+    echo ("Availible appointments are:<br>\t"); 
 
-  while ($rows = mysql_fetch_array($results)){   
-    $key = $rows['key'];
-    $advisor = $rows['advisor'];                                                  
-    $isGroup = $rows['isGroup'];                                                
-    $location = $rows['location'];                                            
-    $dateTime = $rows['dateTime'];                                          
+    echo ("<table>");
+
+    do{
+      $query = "SELECT * FROM `staff` WHERE `key` =".$rows['advisor'];
+      $staffRS = $COMMON->executeQuery($query, $_SERVER["SCRIPT_NAME"]);
+      $staffInfo  = mysql_fetch_array($staffRS);
+
+      $key = $rows['key'];
+      $advisor = ($staffInfo['fname']. " ".$staffInfo['lname']);                                               
+      $isGroup = $rows['isGroup'];                                                
+      $location = $rows['location'];                                            
+      $dateTime = $rows['dateTime'];                                          
       
-    if($isGroup){                                                       
-      echo ("Group ");                                                  
-    }                                                               
-    else{                 
-      echo ("Individual ");}  
-    echo("with ". $advisor. " in ".$location." at ". $dateTime.".");
-    echo("<input type=\"radio\" name=\"appoitment\" value=\"".$key."\"><br>");
-  }  
+      if($isGroup){                                                       
+	$group = "Group";                                 
+      }                                                               
+      else{                 
+	$group = "Individual";}
+
+      echo ("<tr><td>". $group."</td><td>".str_repeat("&nbsp;", 10)."</td>"."<td>". $advisor."</td><td>".str_repeat("&nbsp;", 2)."</td>".
+	    "<td>". $location."</td><td>".str_repeat("&nbsp;", 2)."</td>"."<td>". $dateTime."</td><td>".str_repeat("&nbsp;", 2)."</td>".
+	    "<td><input type=\"radio\" name=\"appoitment\" value=\"".$key."\"></td></tr>");
+
+    } while ($rows = mysql_fetch_array($results));
+    
+    echo ("</table>");
+  
+  }
+  else{
+    echo ("Sorry there are no appointments availible at this time<br>\t");
+  }
 }
 
 ?>

06f329639f28cf1d68b99dd13d6a74795bcf7885
michris1 <cltsd27@gmail.com>
2016-10-13 10:38:22 -0400

    renamed adviser files

---
+++ phpCode/adviserCreateAppointment.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+
+include('./CommonMethods.php');
+$debug = false;
+$COMMON = new Common($debug);
+
+$advisor = mysql_real_escape_string($_SESSION["key"]);
+$isGroup = isset($_POST["cbIsGroup"]) ? 1 : 0;
+$location = mysql_real_escape_string($_POST["tfLocation"]);
+$date = mysql_real_escape_string($_POST["date"]);
+$time = mysql_real_escape_string($_POST["rbTime"]);
+$dateTime = $date . " " . $time . ":00";
+
+$insertMeetingQuery = "INSERT INTO `meetings`(`key`, `advisor`, `isGroup`, `numStudentsRegistered`, `location`, `dateTime`) VALUES ('', $advisor, $isGroup, 0, '$location', '$dateTime')";
+$COMMON->executeQuery($insertMeetingQuery, $_SERVER["SCRIPT_NAME"]);
+
+?>
+<html>
+<head>
+  <title>Meeting Created</title>
+  <link rel="stylesheet" type="text/css" href="../public_html/advising.css">
+</head>
+<body>
+  <h2>Meeting created!</h2>
+  <p>Redirecting back to home page</p>
+</body>
+</html>
+
---
+++ phpCode/adviserCreateApptForm.php
@@ -0,0 +1,61 @@
+<?php
+
+include("VerifySession.php");
+
+$verify = "staffID";
+$redirect = "https://swe.umbc.edu/~michris1/CMSC331/advisingProjectPt1/public_html/staffSignIn.html";
+
+$VERIFY = new Verify($verify, $redirect);
+$VERIFY->verifySession();
+
+
+?>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Meeting Creator</title>
+  <link rel="stylesheet" type="text/css" href="../public_html/advising.css">
+</head>
+<body>
+  <div>
+    <h2>Create Appointment</h2>
+  </div>
+  <div>
+
+    <form method="post" action="adviserCreateAppt.php">
+
+      <label for="cbIsGroup">Group</label><br>
+      <input value="1" type="checkbox" name="cbIsGroup"><br>
+      
+      <label for="tfLocation">Location</label><br>
+      <input type="text" name="tfLocation"><br>
+
+      <label for="date">Date</label><br>
+      <input type="date" name="date"><br>
+
+      <label for="rbTime">Time slot</label><br>
+      <input type="radio" name="rbTime" value="8:30">8:30 - 9:00<br>
+      <input type="radio" name="rbTime" value="9:00">9:00 - 9:30<br>
+      <input type="radio" name="rbTime" value="9:30">9:30 - 10:00<br>
+      <input type="radio" name="rbTime" value="10:00">10:00 - 10:30<br>
+      <input type="radio" name="rbTime" value="10:30">10:30 - 11:00<br>
+      <input type="radio" name="rbTime" value="11:00">11:00 - 11:30<br>
+      <input type="radio" name="rbTime" value="11:30">11:30 - 12:00<br>
+      <input type="radio" name="rbTime" value="12:00">12:00 - 12:30<br>
+      <input type="radio" name="rbTime" value="12:30">12:30 - 1:00<br>
+      <input type="radio" name="rbTime" value="1:00">1:00 - 1:30<br>
+      <input type="radio" name="rbTime" value="1:30">1:30 - 2:00<br>
+      <input type="radio" name="rbTime" value="2:00">2:00 - 2:30<br>
+      <input type="radio" name="rbTime" value="2:30">2:30 - 3:00<br>
+      <input type="radio" name="rbTime" value="3:00">3:00 - 3:30<br>
+      <input type="radio" name="rbTime" value="3:30">3:30 - 4:00<br>
+      <input type="radio" name="rbTime" value="4:00">4:00 - 4:30<br>
+      <input type="radio" name="rbTime" value="4:30">4:30 - 5:00<br>
+
+      <input type="submit" value="Submit">
+
+    </form>
+    
+  </div>
+</body>
+</html>

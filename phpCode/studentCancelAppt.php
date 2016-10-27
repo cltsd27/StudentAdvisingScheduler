099e0876393202b2be5d3a51f086039fdd7e4745
NadezhdaBzhilyanskaya <nadezhda.bzhilyanskaya@gmail.com>
2016-10-26 22:05:46 -0400

    Just added comments

--- phpCode/studentCancelAppt.php
+++ phpCode/studentCancelAppt.php
@@ -9,7 +9,7 @@ include("../public_html/head.html");
 
 $student= mysql_real_escape_string($_SESSION["key"]);
 
-
+//query to check if student already has an appointment
 $currentAppointment = "SELECT * FROM `Appointment` WHERE `Stu1` = " . $student;
 for($i = 2;$i<=10; $i+=1)
 {
@@ -18,12 +18,15 @@ for($i = 2;$i<=10; $i+=1)
 
 $results = $COMMON->executeQuery($currentAppointment, $_SERVER["SCRIPT_NAME"]);
 
+//if there is an appointment print it and give a delete option
 if($rows = mysql_fetch_array($results))
 {
+  // gather advisor infor from current appointment
   $query = "SELECT * FROM `Adviser` WHERE `Key` =".$rows['Adviser'];
   $staffRS = $COMMON->executeQuery($query, $_SERVER["SCRIPT_NAME"]);
   $staffInfo  = mysql_fetch_array($staffRS);
 
+  //get all nessesery data from all queries
   $advisor = ($staffInfo['FirstName']. " ".$staffInfo['LastName']);
   $isGroup = $rows['IsGroup'];
   $location = $rows['Location'];
@@ -32,6 +35,7 @@ if($rows = mysql_fetch_array($results))
   $appoitment= $rows['Key'];
   $stuNum;
   
+  //find which student in the list of 10 is the current one
   for($i = 1;$i<=10; $i++)
   {
     if($student == $rows['Stu'.$i]){$stuNum = $i;}
@@ -40,7 +44,7 @@ if($rows = mysql_fetch_array($results))
   echo("<div class=\"title\"> <h2>Cancel Appointment</h2> </div>");
   echo("<div class=\"content\">");
 
-
+  //print info about appointment
   echo ("<p>Delete this Appointment:<br>");
   if($isGroup){
     echo ("Group ");
@@ -50,19 +54,23 @@ if($rows = mysql_fetch_array($results))
   
   echo("<br>Adviser: ".$advisor. "<br>Location: ".$location."<br>Date: ".$date."<br>Time: ".$time . "</p>");
 
+  //Delete appointment button
   echo ("<form method=\"POST\" action=''>");
 
   echo("<input type=\"submit\" name=\"deleteButton\"  value=\"Delete Appointment\">");
   echo("</form>");
   
+  //Menu button
   echo("<br><a href=\"../public_html/studentHome.html\">Student Home</a>");
 
+  //if the delete button is pressed
   if (isset($_POST['deleteButton'])){
 
     $num  = $rows['NumStu'];
 
     $updateQuery = ("UPDATE `Appointment` SET ");
-
+    
+    //delete the current student from the appointment, shift all students after one over and incriment down the number of students by 1
     for($i = $stuNum;$i <=($num-1);$i++)
     {
       $updateQuery =($updateQuery." `Stu".$i."` = ".$rows['Stu'.($i+1)].",");
@@ -71,11 +79,13 @@ if($rows = mysql_fetch_array($results))
 
 
     $COMMON->executeQuery($updateQuery, $_SERVER["SCRIPT_NAME"]);
+    //go to the menu screen after
     header("Location:  ../public_html/studentHome.html");
     exit();
     }
 
 }
+//if no appointment has been set up go to the view appointment page to let the student know no appointment has been created thus far
 else{
      header("Location: studentViewAppt.php");
      exit();
--- phpCode/studentViewAppt.php
+++ phpCode/studentViewAppt.php
@@ -9,7 +9,7 @@ include("../public_html/head.html");
 
 $student= mysql_real_escape_string($_SESSION["key"]);
 
-
+//Query to check if student is already registared for any appointments
 $currentAppointment = "SELECT * FROM `Appointment` WHERE `Stu1` = " . $student;
 for($i = 2;$i<=10; $i+=1)
 {
@@ -21,6 +21,7 @@ $results = $COMMON->executeQuery($currentAppointment, $_SERVER["SCRIPT_NAME"]);
 echo("<div class=\"title\"> <h2>View Appointment</h2> </div>");
 echo("<div class=\"content\">");
 
+//if there is an appointment display it
 if($rows = mysql_fetch_array($results))
 {
 
@@ -28,12 +29,14 @@ if($rows = mysql_fetch_array($results))
   $staffRS = $COMMON->executeQuery($query, $_SERVER["SCRIPT_NAME"]);
   $staffInfo  = mysql_fetch_array($staffRS);
 
+  //aquaire advisor infor about appointment
   $advisor = ($staffInfo['FirstName']. " ".$staffInfo['LastName']);
   $isGroup = $rows['IsGroup'];
   $location = $rows['Location'];
   $date = $rows['Date'];
   $time = $rows['Time'];
 
+  //print information regarding the appointment
   echo ("You have an appointment set up:<br><br>");
   if($isGroup){
     echo ("Group ");
@@ -45,11 +48,12 @@ if($rows = mysql_fetch_array($results))
   echo("<br><br>");
 
 }
+//if the student doesnt not yet have an appointment state it
 else{
     echo ("Sorry you do not have an appointment yet.<br><br>");
 }
 
-  
+//button to main menu  
 echo("<a href=\"../public_html/studentHome.html\">Student Home</a>");
 
 
